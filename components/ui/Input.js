b7731febc1ff23967c1a17a027977b4e33901620
breaddo <112044280+kylasim@users.noreply.github.com>
2023-09-28 23:20:01 +0800

    datepicker update

--- components/ui/Input.js
+++ components/ui/Input.js
@@ -19,13 +19,9 @@ function CustomInput({onSubmit}) {
         });
       };
 
-      const handleDateChange = (date) => {
-        // Update the Expiry_Date in formData when the datepicker value changes
-        setFormData({
-          ...formData,
-          Expiry_Date: date.toISOString().split("T")[0], // Format the date as "YYYY-MM-DD"
-        });
-      };
+      const [Cdate, setDate] = useState(new Date().toISOString().split("T")[0]); //for expiry date
+
+      console.log(Cdate)
     
       const getCurrentDate = () => {
         const currentDate = new Date();
@@ -41,6 +37,7 @@ function CustomInput({onSubmit}) {
         const updatedFormData = {
           ...formData,
           Creation_Date: getCurrentDate(),
+          Expiry_Date: Cdate
         };
     
         // Call the onSubmit callback with the updated form data
@@ -48,7 +45,7 @@ function CustomInput({onSubmit}) {
       };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} method="POST">
   <div class="mb-6 pt-14">
     <label htmlFor="Role_Name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Role Name</label>
     <input type="text" id="Role_Name" value={formData.Role_Name} onChange={handleInputChange} 
@@ -61,8 +58,10 @@ function CustomInput({onSubmit}) {
   </div>
   <div class="mb-6">
   <label htmlFor="Expiry_Date" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Expiry Date</label>
-    <Datepicker id="Expiry_date" onChange={handleDateChange} selected={formData.Expiry_Date} required
-    class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></Datepicker>
+    <Datepicker id="Expiry_date" dateFormat="YYYY-MM-DD" selected={Cdate} onChange={(date) => {
+  const formattedDate = new Date(date).toISOString().split("T")[0];
+  console.log(formattedDate);
+  setDate(formattedDate);}}/>
   </div>
   <button type="submit" class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800">
   <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
