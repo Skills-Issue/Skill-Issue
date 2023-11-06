426c59698a9545bd9381e184aecc2f2f6991a004
Cytan2000 <tanchiyong00@gmail.com>
2023-11-06 20:54:09 +0800

    UI testing

--- tests/index.test.js
+++ tests/index.test.js
@@ -3,6 +3,8 @@ import { render, screen,fireEvent } from '@testing-library/react';
 import Outline from '@/components/ui/Button';
 import Filter from '@/components/ui/Filter/FilterButton';
 import DismissableModal from '@/components/ui/Filter/Modal';
+import ProfileCard from '@/components/ui/ProfileCard';
+import 'jest-localstorage-mock';
 
 test('renders the Filter button and triggers openModal on click', () => {
   const openModal = jest.fn();
@@ -12,3 +14,23 @@ test('renders the Filter button and triggers openModal on click', () => {
   expect(openModal).toHaveBeenCalledTimes(1);
 });
 
+
+  it('renders the user profile information', () => {
+    localStorage.setItem('user', JSON.stringify({
+      access_role_id:4,
+      country:"Singapore",
+      dept: "HR",
+      email: "Test.Loh@allinone.com.sg",
+      staff_fname:"Test",
+      staff_id:121005,
+      staff_lname:"Loh"
+    }));
+
+    render(<ProfileCard />);
+
+    expect(screen.getByText('Test Loh')).toBeInTheDocument();
+    expect(screen.getByText('Test.Loh@allinone.com.sg')).toBeInTheDocument();
+    expect(screen.getByText('HR')).toBeInTheDocument();
+  });
+
+  
